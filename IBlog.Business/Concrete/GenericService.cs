f1779b38febf926521777e6cea799c70fc634e01
NisanurBulut <nbulut@sarplojistik.com>
2020-12-07 22:15:59 +0300

    implemented core business tier

---
+++ IBlog.Business/Concrete/GenericService.cs
@@ -0,0 +1,59 @@
+﻿using IBlog.DataAccess.Interface;
+using IBlog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IBlog.Business.Concrete
+{
+    public class GenericService<tEntity> : IGenericService<tEntity> where tEntity : class, ITable, new()
+    {
+        private IGenericDal<tEntity> _genericDal;
+        public GenericService(IGenericDal<tEntity> genericDal)
+        {
+            _genericDal = genericDal;
+        }
+
+        public async Task AddAsync(tEntity item)
+        {
+            await _genericDal.AddAsync(item);
+        }
+
+        public async Task<List<tEntity>> GetAllAsync()
+        {
+            return await _genericDal.GetAllAsync();
+        }
+
+        public async Task<List<tEntity>> GetAllAsync(Expression<Func<tEntity, bool>> filter)
+        {
+            return await _genericDal.GetAllAsync(filter);
+        }
+
+        public async Task<List<tEntity>> GetAllAsync<tKey>(Expression<Func<tEntity, tKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(keySelector);
+
+        }
+
+        public async Task<List<tEntity>> GetAllAsync<tKey>(Expression<Func<tEntity, bool>> filter, Expression<Func<tEntity, tKey>> keySelector)
+        {
+            return await _genericDal.GetAllAsync(filter, keySelector);
+        }
+
+        public async Task<tEntity> GetAsync(Expression<Func<tEntity, bool>> filter)
+        {
+            return await _genericDal.GetAsync(filter);
+        }
+
+        public async Task RemoveAsync(tEntity item)
+        {
+            await _genericDal.RemoveAsync(item);
+
+        }
+        public async Task UpdateAsync(tEntity item)
+        {
+            await _genericDal.UpdateAsync(item);
+        }
+    }
+}
---
+++ IBlog.Business/Interface/IGenericService.cs
@@ -0,0 +1,21 @@
+﻿using IBlog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace IBlog.Business
+{
+    public interface IGenericService<tEntity> where tEntity : class, ITable, new()
+    {
+        Task<List<tEntity>> GetAllAsync();
+        Task<List<tEntity>> GetAllAsync(Expression<Func<tEntity, bool>> filter);
+        Task<List<tEntity>> GetAllAsync<tKey>(Expression<Func<tEntity, tKey>> keySelector);
+        Task<List<tEntity>> GetAllAsync<tKey>(Expression<Func<tEntity, bool>> filter, Expression<Func<tEntity, tKey>> keySelector);
+        Task<tEntity> GetAsync(Expression<Func<tEntity, bool>> filter);
+        Task AddAsync(tEntity item);
+
+        Task UpdateAsync(tEntity item);
+        Task RemoveAsync(tEntity item);
+    }
+}
