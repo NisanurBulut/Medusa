10112c4d4109a69cb7b377f0d97bc903ef3cf4a8
NisanurBulut <nbulut@sarplojistik.com>
2020-12-08 23:21:36 +0300

    implemented app user dto

---
+++ Medusa.DataTransferObject/AppUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Medusa.DataTransferObject
+{
+    public class AppUserDto:IDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Medusa.DataTransferObject/AppUserLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medusa.DataTransferObject.Concrete
+{
+    public class AppUserLoginDto:IDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
