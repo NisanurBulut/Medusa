dca433317a14f08a17641cbb897527b8c1418c10
NisanurBulut <nbulut@sarplojistik.com>
2020-12-08 23:27:34 +0300

    implemented auto mapper

---
+++ Medusa.WebAPI/MapProfile/MapProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Medusa.Entities;
+using Medusa.DataTransferObject;
+using Medusa.WebAPI.Models;
+namespace Medusa.WebAPI.MapProfile
+{
+    public class MapProfile :Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<BlogDto, BlogEntity>();
+            CreateMap<BlogEntity, BlogDto>();
+
+            CreateMap<BlogAddModel, BlogEntity>();
+            CreateMap<BlogEntity, BlogAddModel>();
+
+            CreateMap<BlogUpdateModel, BlogEntity>();
+            CreateMap<BlogEntity, BlogUpdateModel>();
+
+            CreateMap<BlogDto, BlogEntity>();
+            CreateMap<BlogEntity, BlogDto>();
+
+            CreateMap<CategoryAddDto, CategoryEntity>();
+            CreateMap<CategoryEntity, CategoryAddDto>();
+
+            CreateMap<CategoryDto, CategoryEntity>();
+            CreateMap<CategoryEntity, CategoryDto>();
+
+            CreateMap<CategoryUpdateDto, CategoryEntity>();
+            CreateMap<CategoryEntity, CategoryUpdateDto>();
+        }
+    }
+}
