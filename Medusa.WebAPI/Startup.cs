27ff60327f6a33606ad60c5bff4b3a1e33d1e794
NisanurBulut <nbulut@sarplojistik.com>
2020-12-10 00:09:45 +0300

    set cinfiguration json for uploading image

--- Medusa.WebAPI/Startup.cs
+++ Medusa.WebAPI/Startup.cs
@@ -22,7 +22,10 @@ namespace Medusa.WebAPI
         {
             services.AddAutoMapper(typeof(Startup));
             services.AddDependecy();
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
