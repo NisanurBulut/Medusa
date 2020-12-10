55241b05bc4a375d43df736c357af3f8e3e86a3c
NisanurBulut <nbulut@sarplojistik.com>
2020-12-10 19:55:54 +0300

    implemented blog list model

---
+++ Medusa.Web/Models/BlogListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medusa.WebUI.Models
+{
+    public class BlogListModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public DateTime PostedTime { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
