463037c6c6864b203df45ef71949aa3b36708002
NisanurBulut <nbulut@sarplojistik.com>
2020-12-12 21:31:51 +0300

    implemented jwt configuration

---
+++ Medusa.Business/StringInfo/JwtInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medusa.Business.StringInfo
+{
+    public class JwtInfo
+    {
+        public const string ISSUER = "http://localhost:55315/";
+        public const string AUDIENCE= "http://localhost:55555/";
+        public const string SECURITYKEY = "nisanurrunasin";
+    }
+}
--- Medusa.WebAPI/Startup.cs
+++ Medusa.WebAPI/Startup.cs
@@ -1,10 +1,15 @@
 using AutoMapper;
 using Medusa.Business.Containers;
+using Medusa.Business.StringInfo;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
 
 namespace Medusa.WebAPI
 {
@@ -22,6 +27,22 @@ namespace Medusa.WebAPI
         {
             services.AddAutoMapper(typeof(Startup));
             services.AddDependecy();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    // https kapatýlýr
+                    opt.RequireHttpsMetadata = false;
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = JwtInfo.ISSUER,
+                        ValidAudience = JwtInfo.AUDIENCE,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtInfo.SECURITYKEY)),
+                        ValidateLifetime = true, // token süresi geçince geçersiz sayýlsýn
+                        ValidateAudience = true, // baþka adresten istek gelrse geçersiz kýl
+                        ValidateIssuer = true,
+                        ClockSkew = TimeSpan.Zero // sunucular arasýnda zaman farký koyma
+                    };
+                });
             services.AddControllers().AddNewtonsoftJson(opt =>
             {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
